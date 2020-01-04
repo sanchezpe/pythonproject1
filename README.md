569ca6085c4c19c5b19e69f9210d28ab0c1e1b30
Pablo <pabloesanchez1@gmail.com>
2020-01-04 11:51:23 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Programming Assignment #1
+
+## Description
+This program will compute the gratuity and final bill for a restaurant meal.
+
+## Sample Run
+	Enter customer name: George Snow
+	Enter subtotal: 46.85
+	Enter gratuity: 18
+	Customer: George Snow
+	Subtotal: 46.85
+	Gratuity: 8.433
+	Final Bill: 55.283
---
+++ pa1.py
@@ -0,0 +1,28 @@
+﻿# ask the customer for their name using an input statement
+nameCustomer = input ("Enter customer name: ")
+
+
+# ask the user for their bill subtotal (before gratuity) using an input statement
+# remember to “eval” the variable in order to perform arithmetic operations later
+subtotal = eval(input("Enter subtotal: "))
+
+
+# ask the user for the gratuity percent, e.g., 15 (they will not enter the % sign)
+# use an input statement
+# again, remember to “eval” the variable
+gratuityPercent = eval(input("Enter gratuity: "))
+
+
+# compute the gratuity charge, which is bill subtotal * (gratuity percent/100)
+gratuityCharge = (subtotal * (gratuityPercent / 100))
+
+
+# compute the final bill, which is subtotal + gratuity charge
+total = (subtotal + gratuityCharge)
+
+
+# print all input and computed values
+print ("Customer:    " , nameCustomer)
+print ("Subtotal:    " , subtotal)
+print ("Gratuity:    " , gratuityCharge)
+print ("Final Bill:  " , total)
